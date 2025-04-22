Murata-MEMS/murata-sensor-driver

commit 2e0379a1d8fd31c7f84ff138879946a7a3b10c7d

Reset the sensor always in the beginning before starting the initialization.
Hard reset with EXTRESN resolves a possible problem in starting the ASIC, if
power supplies or IO lines weren't stable at power up.

drivers/iio/imu/murata/sch16xx.c
@@ -382,7 +382,7 @@ static const struct filter_params *find_filter_params_hz(const struct filter_par
 
 		if (params[i].cutoff_hz == cutoff_freq) {
 			return &params[i];
-		}	
+		}
 	}
 
 	return NULL;
@@ -719,11 +719,6 @@ static int sch16xx_init(struct iio_dev *indio_dev)
 	int retry;
 	for (retry = 0; retry < 3; retry++) {
 
-		ret = sch16xx_reset(indio_dev);
-		if (ret)
-			return ret;
-		msleep(32);
-
 		// rate filter
 		// do not check the first frame CRC after reset
 		filt = chip->rate_filter;
@@ -799,10 +794,16 @@ static int sch16xx_init(struct iio_dev *indio_dev)
 		if (ret)
 			return ret;
 
-		if (status_failed)
+		if (status_failed) {
 			dev_warn(&indio_dev->dev, "Fault in status registers, start attept %d.", retry + 1);
-		else
+
+			ret = sch16xx_reset(indio_dev);
+			if (ret)
+				return ret;
+			msleep(32);
+		} else {
 			return 0;
+		}
 	}
 	dev_err(&indio_dev->dev, "Failed to start the sensor.");
 	return -EIO;
@@ -1337,6 +1338,12 @@ static int sch16xx_probe (struct spi_device *spi)
 	}
 
 	chip->vddio_1v8 = of_property_read_bool(spi->dev.of_node, "murata,vddio_1v8");
+
+	ret = sch16xx_reset(iio_dev);
+	if (ret)
+		return ret;
+
+	msleep(32);
 
 	{
 		unsigned int id;

commit 30ce6f81feeaf3f69ff786e706f4f1b91323b8e0

The sensor must be reset before altering sensor settings.
When EOI=1 the settings are write protected. A change in commit 2e0379a
caused the first reset to be skipped in sch16xx_init.

drivers/iio/imu/murata/sch16xx.c
@@ -686,7 +686,7 @@ static void sch16xx_reset(struct iio_dev *indio_dev)
 	gpiod_set_value_cansleep(chip->reset_gpio, 1);
 }
 
-static int sch16xx_init(struct iio_dev *indio_dev)
+static int sch16xx_init(struct iio_dev *indio_dev, bool do_reset)
 {
 	unsigned int value;
 	struct sch16xx_dev *chip = iio_priv(indio_dev);
@@ -694,6 +694,12 @@ static int sch16xx_init(struct iio_dev *indio_dev)
 	const struct filter_params *filt;
 	int ret;
 	bool status_failed;
+	int retry;
+
+	if (do_reset) {
+		sch16xx_reset(indio_dev);
+		msleep(32);
+	}
 
 	// Start up sequence
 	//   power on
@@ -711,7 +717,6 @@ static int sch16xx_init(struct iio_dev *indio_dev)
 	//     reset if not OK ->
 	// Done
 
-	int retry;
 	for (retry = 0; retry < 3; retry++) {
 
 		// rate filter
@@ -900,7 +905,7 @@ static int sch16xx_write_raw(struct iio_dev *indio_dev, struct iio_chan_spec con
 	}
 
 	// Reinit the sensor to use the new settings
-	ret = sch16xx_init(indio_dev);
+	ret = sch16xx_init(indio_dev, true);
 	
 	return ret;
 }
@@ -1024,7 +1029,7 @@ static ssize_t rate_dyn_store(struct device *dev, struct device_attribute *attr,
 	chip->rate_range = dyn;
 
 	// Reinit the sensor to use the new settings
-	ret = sch16xx_init(indio_dev);
+	ret = sch16xx_init(indio_dev, true);
 	if (ret < 0)
 		return ret;
 
@@ -1056,7 +1061,7 @@ static ssize_t accel_dyn_store(struct device *dev, struct device_attribute *attr
 	chip->acc12_range = dyn;
 
 	// Reinit the sensor to use the new settings
-	ret = sch16xx_init(indio_dev);
+	ret = sch16xx_init(indio_dev, true);
 	if (ret < 0)
 		return ret;
 
@@ -1216,7 +1221,7 @@ static int sch16xx_suspend(struct device *dev)
 static int sch16xx_resume(struct device *dev)
 {
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
-	return sch16xx_init(indio_dev);
+	return sch16xx_init(indio_dev, true);
 }
 #endif
 
@@ -1341,8 +1346,8 @@ static int sch16xx_probe (struct spi_device *spi)
 
 	chip->vddio_1v8 = of_property_read_bool(spi->dev.of_node, "murata,vddio_1v8");
 
+	// Do hard reset before any SPI communication, in case the ASIC is stuck
 	sch16xx_reset(iio_dev);
-	
 	msleep(32);
 
 	{
@@ -1352,9 +1357,12 @@ static int sch16xx_probe (struct spi_device *spi)
 			goto err;
 		chip->comp_id = id;
 		chip->product_code = find_product_code(id);
+
+		sch16xx_read_single(chip, REG_CTRL_MODE, &id, true);
+		dev_dbg(&spi->dev, "mode=%d", id);
 	}
 
-	ret = sch16xx_init(iio_dev);
+	ret = sch16xx_init(iio_dev, false);
 	if (ret)
 		goto err;
 